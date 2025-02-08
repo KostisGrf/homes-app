f41dffda60aac3fe28555a31596457dbb636358f
Kostis Grf <kostisgratsos@hotmail.gr>
2025-02-08 04:31:35 +0200

    Added angular forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -115,4 +115,8 @@ export class HousingService {
   getHousingLocationById(id:number):Housinglocation | undefined{
      return this.housingLocationList.find(housingLocation=>housingLocation.id===id);
   }
+
+  submitApplication(firstName:string,lastName:string,email:string){
+    console.log(firstName,lastName,email)
+  }
 }
